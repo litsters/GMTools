968ae0abf674016c017097ea625d411a1a1b8e5a
Ryan Penfold <rpenfold@live.com>
2018-10-11 10:35:33 -0600

    main router now uses a router configuration to reduce maintenance

--- app/src/scripts/react/routers/main-router.js
+++ app/src/scripts/react/routers/main-router.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
-import LoginPage from "../pages/login/LoginPage";
+import { MainConfig as Config } from "./config";
 
+import LoginPage from "../pages/login/LoginPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+
+// used to map configuration to components
+const components = { LoginPage, NotFoundPage };
 
 class MainRouter extends Component {
     render() {
@@ -9,15 +15,18 @@ class MainRouter extends Component {
 
         return (
             <Switch>
+                {Object.keys(Config.routes).map((key) => {
+                    let route = Config.routes[key],
+                        Page = components[route.component];
 
-                <Route path="/login" render={({match}) => {
-                    return <LoginPage />
-                }} />
-
-                <Route path="" render={({match}) => {
-                    return <div history={history}>main path</div>
-                }} />
-
+                    return (
+                        <Route key={key} path={route.path} render={({match}) => {
+                            console.log(route);
+                            return <Page history={history} match={match} />
+                            
+                        }} />
+                    );
+                })}
             </Switch>
         );
     }
