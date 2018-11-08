15f17cb1e337e2270a57859bf22e31986b2c046b
litsters <snlitster@gmail.com>
2018-11-08 11:49:19 -0700

    server get working, .gitignore updated

--- server/dist/api/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var auth_1 = require("./auth");
-exports.AuthApi = auth_1.default;
-var plugin_1 = require("./plugin");
-exports.PluginApi = plugin_1.default;
-//# sourceMappingURL=index.js.map
