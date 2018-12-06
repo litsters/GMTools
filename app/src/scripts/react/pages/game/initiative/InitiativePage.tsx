80bee48697bec5abd433c3536073340ff7b41b21
Curtis Oakley <coakley@chocklydigital.com>
2018-12-06 08:36:27 -0700

    Allow for characters to be added from a campaign.

--- app/src/scripts/react/pages/game/initiative/InitiativePage.tsx
+++ app/src/scripts/react/pages/game/initiative/InitiativePage.tsx
@@ -1,47 +1,56 @@
 import React, { Component } from "react";
 import IPage from "../../../interfaces/IPage";
+import EventBus from "../../../common/Events";
+import { each } from "jquery"
+import { Campaign, Character } from "../../../interfaces";
 
 interface Initiative {
     key: number,
     initiative: number
     name: string
-    character?: string
+    user?: string // Optional user id for the initiative item
 }
 
 interface InitiativePageState {
     initiative: Initiative[]
     addName: string
     addInitiative: string
+    characters: Character[]
+    campaigns: Campaign[]
+    selectedCampaign?: string
 }
 
 class InitiativePage extends Component<IPage, InitiativePageState> {
     private keyCounter: number;
+    private charactersList: Character[];
+    private events: EventBus;
+    private readonly eventListeners: object;
 
     constructor(props: any) {
         super(props);
 
+        // Declare what events we listen to and what method will handle them
+        this.eventListeners = {
+            'data.retrieved': this.dataRetrieved.bind(this),
+            'data.persisted': this.dataPersisted.bind(this),
+        };
+
+        // Bind events
+        EventBus.get()
+            .then((bus) => {
+                this.events = bus;
+
+                each(this.eventListeners, (event, callback) => {
+                    this.events.on(event, callback);
+                });
+            });
+
         this.state = {
-            initiative: [
-                {
-                    key: 0,
-                    initiative: 1,
-                    name: 'Bob',
-                    character: '',
-                },
-                {
-                    key: 1,
-                    initiative: 16,
-                    name: 'Dragon',
-                },
-                {
-                    key: 2,
-                    initiative: 22,
-                    name: 'Jester',
-                    character: '',
-                },
-            ],
+            initiative: [],
             addName: '',
             addInitiative: '',
+            characters: [],
+            campaigns: [],
         };
 
         this.next = this.next.bind(this);
@@ -49,6 +58,74 @@ class InitiativePage extends Component<IPage, InitiativePageState> {
         this.updateInitiative = this.updateInitiative.bind(this);
 
         this.keyCounter = this.state.initiative.length;
+        this.charactersList = [];
+    }
+
+    componentDidMount() {
+        EventBus.emit('data.get', {namespace: 'campaign', key: 'get_campaigns'}, true);
+        EventBus.emit('data.get', {namespace: 'character', key: 'get_characters'}, true);
+    }
+
+    componentWillUnmount() {
+        if (this.events !== null) {
+            each(this.eventListeners, (event, callback) => {
+                this.events.removeListener(event, callback);
+            });
+        }
+    }
+
+    dataRetrieved(event: any) {
+        if (event.namespace === "character" && event.key === "get_characters" && event.data) {
+            // Character data retrieved save
+            this.charactersList = event.data;
+
+        } else if (event.namespace === "campaign" && event.key === "get_campaigns" && event.data) {
+            // Campaign data retrieved, save into the state
+            this.setState({
+                campaigns: event.data,
+            });
+        }
+    }
+
+    dataPersisted(event: any) {
+        // TODO campaigns and character state should be centralized into redux so no individual component has to deal with it
+        if (event.namespace === "campaign" && event.key === "new_campaign" && event.data) {
+            const campaigns = this.state.campaigns.slice();
+            campaigns.push(event.data);
+            this.setState({
+                campaigns: campaigns,
+            });
+        } else if (event.namespace === "character" && event.data) {
+            if (event.key === "new_character") {
+                // New character added, save
+                this.charactersList.push(event.data);
+
+            } else if (event.key === "character_updated") {
+                // Character updated, update that character
+                this.charactersList = this.charactersList.map((character: Character) => {
+                    if (character._id === event.data._id) {
+                        return event.data;
+                    }
+                    return character;
+                });
+            }
+        }
+    }
+
+    addCharacterList() {
+        // Adds all characters from the state to the initiative
+        let initiative = this.state.initiative.slice();
+        this.state.characters.forEach((character) => {
+            initiative.push({
+                key: ++this.keyCounter,
+                initiative: 0,
+                name: character.name,
+                user: character.user,
+            });
+        });
+        this.setState({
+            initiative: initiative,
+        })
     }
 
     next() {
@@ -108,6 +185,28 @@ class InitiativePage extends Component<IPage, InitiativePageState> {
         });
     }
 
+    updateSelectedCampaign(event: any): void {
+        let selectedCampaign: Campaign;
+        let characters: Character[] = [];
+        this.state.campaigns.forEach((campaign: Campaign) => {
+            if (campaign._id == event.target.value) {
+                selectedCampaign = campaign;
+            }
+        });
+        this.charactersList.forEach((character: Character) => {
+            selectedCampaign.characters.forEach((characterId: string) => {
+                if (characterId === character._id) {
+                    characters.push(character);
+                }
+            });
+        });
+
+        this.setState({
+            characters: characters,
+            selectedCampaign: selectedCampaign._id,
+        });
+    }
+
     static swap(arr: any[], i1: number, i2: number) {
         let swap = arr[i2];
         arr[i2] = arr[i1];
@@ -146,25 +245,45 @@ class InitiativePage extends Component<IPage, InitiativePageState> {
         return (
             <div>
                 <h1>Initiative</h1>
-                <ol className="initiativeList">
-                    {this.state.initiative.map((a, i) => {
-                        return (
-                            <li key={a.key}>
-                                <span>{a.name}</span>
-                                <input type="text" value={a.initiative} onChange={(event) => this.updateInitiative(i, event)}/>
-                                <button type="button" onClick={() => this.remove(i)}>-</button>
-                                {i > 0 && <button type="button" onClick={() => this.moveUp(i)}>^</button> }
-                                {i < this.state.initiative.length - 1 && <button type="button" onClick={() => this.moveDown(i)}>v</button> }
-                            </li>
-                        );
-                    })}
-                </ol>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" placeholder="Name" value={this.state.addName} onChange={(event) => this.updateInputState('addName', event)}/>
-                    <input type="text" value={this.state.addInitiative} onChange={(event) => this.updateInputState('addInitiative', event, true)}/>
-                    <button type="submit" title="Add to initiative">+</button>
-                </form>
-                <button type="button" onClick={this.next}>Next</button>
+                <div>
+                    <ol className="initiativeList">
+                        {this.state.initiative.map((a, i) => {
+                            return (
+                                <li key={a.key}>
+                                    <span>{a.name}</span>
+                                    <input type="text" value={a.initiative} onChange={(event) => this.updateInitiative(i, event)}/>
+                                    <button type="button" onClick={() => this.remove(i)}>-</button>
+                                    {i > 0 && <button type="button" onClick={() => this.moveUp(i)}>^</button> }
+                                    {i < this.state.initiative.length - 1 && <button type="button" onClick={() => this.moveDown(i)}>v</button> }
+                                </li>
+                            );
+                        })}
+                    </ol>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Name" value={this.state.addName} onChange={(event) => this.updateInputState('addName', event)}/>
+                        <input type="text" value={this.state.addInitiative} onChange={(event) => this.updateInputState('addInitiative', event, true)}/>
+                        <button type="submit" title="Add to initiative">+</button>
+                    </form>
+                    <button type="button" onClick={this.next}>Next</button>
+                </div>
+                <div>
+                    <div>
+                        <select onChange={(event) => this.updateSelectedCampaign(event)}>
+                            <option value=""> </option>
+                            {this.state.campaigns.map((campaign: Campaign) => {
+                                return (<option key={campaign._id} value={campaign._id}>{campaign.name}</option>)
+                            })}
+                        </select>
+                    </div>
+                    <div>
+                        <ul>
+                            {this.state.characters.map((character: Character) => {
+                                return (<li key={character._id}>{character.name}</li>)
+                            })}
+                        </ul>
+                        <button type="button" onClick={() => this.addCharacterList()}>Add All</button>
+                    </div>
+                </div>
             </div>
         );
     }
